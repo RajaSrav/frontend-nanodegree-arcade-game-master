9dbffcf09536fe42fda0822d9fb4d792ee2a9ec3
GKRaja33 <kondapalli.g@apssdc.in>
2019-05-09 10:12:19 +0530

    complete arcade game

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# frontend-nanodegree-arcade-game-master
-frontend-nanodegree-arcade-game-master
+# Classic Arcade Game Clone Project
+
+The main moto of the game is to make the player to go to the water area without touching the enemies. And do an activity after the player reached the water area.
+
+## Table of Contents
+
+1.css folder
+2.images folder
+3.js folder
+4.index.html files
+5.README.md file
+
+-   [Instructions](#instructions)
+-   [Contributing](#contributing)
+    ## Procedure to download the scratch project from the git hub
+
+
+-   This game can be repository on github: <https://github.com/udacity/frontend-nanodegree-arcade-game>.
+-   Download from the above github link.
+-   Now extract the folder.
+    ## Instructions
+
+Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+
+Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+
+Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+## Steps involved in this project.
+
+Step 1 :- Here I resolved the bugs that given in starter code, Then I got canvas.
+Step 2 :- I applied handleInput() method for player controlling through keyboard.
+Step 3 :- I created a array(allEnemies\[]) and updated more than two enemies in this array.
+Step 4 :- After that I provided speed and different position for enemies in initial stage of the game.
+Step 5 :- We need to check this logic (if Player can not move the out of screen and if collision occurred player got into their initial position and player reached destination then we got a alert and placed the player in initial position).
+
+## Procedure to play this game.
+
+1.  Open index.html file in any browser.
+2.  Move player position by using Arrow Keys (left, right, up, down) from keyboard.
+3.  Left and right Keys for player moving lift side and right side meanwhile up and down keys is for player moving up and down.
+4.  The challenge is to avoid the bugs that travel on the road. If a bug is touched, the character is reset back at the beginning. Upon reaching the water, the user is also reset back to the beginning.
+5.  Player need to go grass to water (Bottom to top) without touching enemies bugs then got a alert message and player got their initial position in screen.
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
---
+++ js/app.js
@@ -0,0 +1,99 @@
+// strict method is used for detecting undeclared variables and code should be run in strict mode.
+'use strict';
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+  // You should multiply any movement by the dt parameter
+  this.x = this.x + this.speed * dt;
+  while (this.x > 505) {
+    this.x = 0
+  }
+  if (player.x < this.x + 60 && player.x + 60 > this.x && player.y < this.y + 60 && player.y + 70 > this.y) {
+    player.x = 202;
+    player.y = 404;
+  }
+  // which will ensure the game runs at the same speed for
+  // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// Place the Player
+let Player = function(x, y) {
+  this.sprite = "images/char-boy.png";
+  this.x = 202;
+  this.y = 404;
+
+};
+let player = new Player();
+// This class requires an update(), render() and
+Player.prototype.update = function(dt) {
+
+};
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// a handleInput() method.
+Player.prototype.handleInput = function(directions) {
+  // Right keey press move right side 100 steps (one box move)
+  if (directions === "right" && this.x < 400) {
+    this.x += 100;
+  }
+  // left keey press move left side -101 steps (left side one box move)
+  if (directions === "left" && this.x > 0) {
+    this.x -= 101;
+  }
+  // up keey press move up direction -100 steps (one box move)
+  if (directions === "up" && this.y > 0) {
+    this.y -= 100;
+  }
+  // down keey press move down 100 steps (one box move)
+  if (directions === "down" && this.y < 404) {
+    this.y += 100;
+  }
+  if (this.y < 0) {
+    this.x = 202;
+    this.y = 404;
+    alert("congratulations your won the Game");
+  }
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+let allEnemies = [];
+// Place the player object in a variable called player
+let allEnemiesPosition = [60, 140, 230];
+// map function for generating enemies in different position
+allEnemiesPosition.map((direction) => {
+  //  Math.random() method
+  let min = 0;
+  let max = 333;
+  var enemy = new Enemy(min, direction, 100 + Math.floor(Math.random() * max));
+  allEnemies.push(enemy);
+})
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
+});
